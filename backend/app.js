9f14fe142588a96137339a6b384c61aa910dfa75
66160115 <66160115@go.buu.ac.th>
2025-11-14 18:09:32 +0700

    feat: add CORS support and environment configuration

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,16 @@ import express from "express";
 import authRouter from "./src/routes/authRoute.js";
 import ticketRouter from "./src/routes/ticketRoute.js";
 import userRoute from "./src/routes/userRoute.js";
+import cors from "cors";
 
 const app = express()
 const port = 3000
 
-app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:4000",
+}));
 
+app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/tickets", ticketRouter);
 app.use("/api/users", userRoute);
